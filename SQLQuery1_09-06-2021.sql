ffaed5f0314eccb4236d10462352b8b1835c95d0
TarunSurapaneni <66796241+tarunsurapaneni412@users.noreply.github.com>
2021-09-06 15:09:16 -0700

    Add files via upload

---
+++ SQLQuery1_09-06-2021.sql
@@ -0,0 +1,572 @@
+--Question 1
+/*Several friends at a cinema ticket office would like to reserve consecutive available seats.
+Can you help to query all the consecutive available seats order by the seat_id using the following cinema table?
+| seat_id | free |
+|---------|------|
+| 1       | 1    |
+| 2       | 0    |
+| 3       | 1    |
+| 4       | 1    |
+| 5       | 1    |
+ 
+
+Your query should return the following result for the sample case above.
+ 
+
+| seat_id |
+|---------|
+| 3       |
+| 4       |
+| 5       |
+Note:
+The seat_id is an auto increment int, and free is bool ('1' means free, and '0' means occupied.).
+Consecutive available seats are more than 2(inclusive) seats consecutively available.*/
+
+--Solution:
+
+with t as
+(
+Select *, lead(free) over (order by seat_id) as free_1, lag(free) over (order by seat_id) as 'free_0'
+from cinema
+)
+select seat_id
+from t
+where  (free = 1 and free_1 = 1) or (free=1 and free_0 =1) 
+order by seat_id;
+
+---------------------------------------------------------------------------------------------------------------------------------------
+--Question 2
+/*Description
+
+Given three tables: salesperson, company, orders.
+Output all the names in the table salesperson, who didn’t have sales to company 'RED'.
+
+Example
+Input
+
+Table: salesperson
+
++----------+------+--------+-----------------+-----------+
+| sales_id | name | salary | commission_rate | hire_date |
++----------+------+--------+-----------------+-----------+
+|   1      | John | 100000 |     6           | 4/1/2006  |
+|   2      | Amy  | 120000 |     5           | 5/1/2010  |
+|   3      | Mark | 65000  |     12          | 12/25/2008|
+|   4      | Pam  | 25000  |     25          | 1/1/2005  |
+|   5      | Alex | 50000  |     10          | 2/3/2007  |
++----------+------+--------+-----------------+-----------+
+The table salesperson holds the salesperson information. Every salesperson has a sales_id and a name.
+Table: company
+
++---------+--------+------------+
+| com_id  |  name  |    city    |
++---------+--------+------------+
+|   1     |  RED   |   Boston   |
+|   2     | ORANGE |   New York |
+|   3     | YELLOW |   Boston   |
+|   4     | GREEN  |   Austin   |
++---------+--------+------------+
+The table company holds the company information. Every company has a com_id and a name.
+Table: orders
+
++----------+------------+---------+----------+--------+
+| order_id | order_date | com_id  | sales_id | amount |
++----------+------------+---------+----------+--------+
+| 1        |   1/1/2014 |    3    |    4     | 100000 |
+| 2        |   2/1/2014 |    4    |    5     | 5000   |
+| 3        |   3/1/2014 |    1    |    1     | 50000  |
+| 4        |   4/1/2014 |    1    |    4     | 25000  |
++----------+----------+---------+----------+--------+
+The table orders holds the sales record information, salesperson and customer company are represented by sales_id and com_id.
+output
+
++------+
+| name | 
++------+
+| Amy  | 
+| Mark | 
+| Alex |
++------+
+Explanation
+
+According to order '3' and '4' in table orders, it is easy to tell only salesperson 'John' and 'Pam' have sales to company 'RED',
+so we need to output all the other names in the table salesperson.*/
+
+--Solution
+
+Select name from Salesperson where sales_id not in 
+                  (Select distinct O.sales_id from Salesperson S left join orders O on S.sales_id = O.Sales_id 
+                                                                 left Join company C on O.com_id = C.com_id 
+where C.name = 'RED')
+
+---------------------------------------------------------------------------------------------------------------------------------------
+--Question 3 
+ /*A pupil Tim gets homework to identify whether three line segments could possibly form a triangle.
+ 
+
+However, this assignment is very heavy because there are hundreds of records to calculate.
+ 
+
+Could you help Tim by writing a query to judge whether these three sides can form a triangle, assuming table triangle holds the length of the three sides x, y and z.
+ 
+
+| x  | y  | z  |
+|----|----|----|
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
+For the sample data above, your query should return the follow result:
+| x  | y  | z  | triangle |
+|----|----|----|----------|
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |*/
+
+--Solution 
+
+Select *,
+case 
+     When x+y >z AND x+z >y AND y+z > x THEN 'Yes'
+     Else 'No'
+END AS triangle
+From triangle 
+
+---------------------------------------------------------------------------------------------------------------------------------------
+--Question 4 
+/*
+Table my_numbers contains many numbers in column num including duplicated ones.
+Can you write a SQL query to find the biggest number, which only appears once.
+
++---+
+|num|
++---+
+| 8 |
+| 8 |
+| 3 |
+| 3 |
+| 1 |
+| 4 |
+| 5 |
+| 6 | 
+For the sample data above, your query should return the following result:
++---+
+|num|
++---+
+| 6 |
+*/
+
+--Solution 4
+select max(num) as num from (
+select
+num
+from my_numbers
+group by num
+having count(num) = 1
+) abc
+
+--------------------------------------------------------------------------------------------------------------------------------------
+--Question 5:
+/*Table: Cinema
+
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| id             | int      |
+| movie          | varchar  |
+| description    | varchar  |
+| rating         | float    |
++----------------+----------+
+id is the primary key for this table.
+Each row contains information about the name of a movie, its genre, and its rating.
+rating is a 2 decimal places float in the range [0, 10]
+ 
+
+Write an SQL query to report the movies with an odd-numbered ID and a description that is not "boring".
+
+Return the result table in descending order by rating.
+
+The query result format is in the following example:
+
+ 
+
+Cinema table:
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 1  | War        | great 3D    | 8.9    |
+| 2  | Science    | fiction     | 8.5    |
+| 3  | irish      | boring      | 6.2    |
+| 4  | Ice song   | Fantacy     | 8.6    |
+| 5  | House card | Interesting | 9.1    |
++----+------------+-------------+--------+
+
+Result table:
++----+------------+-------------+--------+
+| id | movie      | description | rating |
++----+------------+-------------+--------+
+| 5  | House card | Interesting | 9.1    |
+| 1  | War        | great 3D    | 8.9    |
++----+------------+-------------+--------+
+
+We have three movies with odd-numbered ID: 1, 3, and 5. The movie with ID = 3 is boring so we don't include it in the answer.*/
+
+--Solution 
+Select * from Cinema
+where id%2 != 0
+      and description != 'boring'
+order by rating desc
+
+--------------------------------------------------------------------------------------------------------------------------------------
+--Question 6:
+/*Table: Salary
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| id          | int      |
+| name        | varchar  |
+| sex         | ENUM     |
+| salary      | int      |
++-------------+----------+
+id is the primary key for this table.
+The sex column is ENUM value of type ('m', 'f').
+The table contains information about an employee.
+ 
+
+Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement and no intermediate temp table(s).
+
+Note that you must write a single update statement, DO NOT write any select statement for this problem.
+
+The query result format is in the following example:
+
+ 
+
+Salary table:
++----+------+-----+--------+
+| id | name | sex | salary |
++----+------+-----+--------+
+| 1  | A    | m   | 2500   |
+| 2  | B    | f   | 1500   |
+| 3  | C    | m   | 5500   |
+| 4  | D    | f   | 500    |
++----+------+-----+--------+
+
+Result table:
++----+------+-----+--------+
+| id | name | sex | salary |
++----+------+-----+--------+
+| 1  | A    | f   | 2500   |
+| 2  | B    | m   | 1500   |
+| 3  | C    | f   | 5500   |
+| 4  | D    | m   | 500    |
++----+------+-----+--------+
+(1, A) and (3, C) were changed from 'm' to 'f'.
+(2, B) and (4, D) were changed from 'f' to 'm'.*/
+
+--Solution:
+UPDATE Salary
+SET sex = (CASE WHEN sex = 'f' THEN 'm' ELSE 'f' END )
+
+--------------------------------------------------------------------------------------------------------------------------------------
+--Question 7:
+/*
+Table: ActorDirector
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| actor_id    | int     |
+| director_id | int     |
+| timestamp   | int     |
++-------------+---------+
+timestamp is the primary key column for this table.
+ 
+
+Write a SQL query for a report that provides the pairs (actor_id, director_id) where the actor have cooperated with the director at least 3 times.
+
+Example:
+
+ActorDirector table:
++-------------+-------------+-------------+
+| actor_id    | director_id | timestamp   |
++-------------+-------------+-------------+
+| 1           | 1           | 0           |
+| 1           | 1           | 1           |
+| 1           | 1           | 2           |
+| 1           | 2           | 3           |
+| 1           | 2           | 4           |
+| 2           | 1           | 5           |
+| 2           | 1           | 6           |
++-------------+-------------+-------------+
+
+Result table:
++-------------+-------------+
+| actor_id    | director_id |
++-------------+-------------+
+| 1           | 1           |
++-------------+-------------+
+The only pair is (1, 1) where they cooperated exactly 3 times.
+*/
+
+--Solution 
+Select actor_id, director_id from ActorDirector 
+group by actor_id,director_id 
+having count(*)>=3
+
+-------------------------------------------------------------------------------------------------------------------------------------
+--Question 8:
+/*
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key of this table.
+product_id is a foreign key to Product table.
+Note that the price is per unit.
+ 
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key of this table.
+ 
+
+Write an SQL query that reports the product_name, year, and price for each sale_id in the Sales table.
+
+Return the resulting table in any order.
+
+The query result format is in the following example:
+
+ 
+
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+
+Result table:
++--------------+-------+-------+
+| product_name | year  | price |
++--------------+-------+-------+
+| Nokia        | 2008  | 5000  |
+| Nokia        | 2009  | 5000  |
+| Apple        | 2011  | 9000  |
++--------------+-------+-------+
+From sale_id = 1, we can conclude that Nokia was sold for 5000 in the year 2008.
+From sale_id = 2, we can conclude that Nokia was sold for 5000 in the year 2009.
+From sale_id = 7, we can conclude that Apple was sold for 9000 in the year 2011.
+*/
+
+--Solution 
+Select P.product_name, S.year, S.price from Sales S 
+                                       Left Join Product P
+                                       on S.product_id = P.product_id
+
+--------------------------------------------------------------------------------------------------------------------------------------                                                
+--Question 9
+/*
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+sale_id is the primary key of this table.
+product_id is a foreign key to Product table.
+Note that the price is per unit.
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key of this table.
+ 
+
+Write an SQL query that reports the total quantity sold for every product id.
+
+The query result format is in the following example:
+
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+
+Result table:
++--------------+----------------+
+| product_id   | total_quantity |
++--------------+----------------+
+| 100          | 22             |
+| 200          | 15             |
++--------------+----------------+
+*/
+
+--Solution 
+Select product_id, Sum(quantity) as total_quantity 
+from Sales 
+group by product_id
+
+---------------------------------------------------------------------------------------------------------------------------------------
+--Question 10:
+/*
+Write a SQL query to get the nth highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the nth highest salary where n = 2 is 200. If there is no nth highest salary, then the query should return null.
+
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+*/
+
+--Solution 
+
+CREATE FUNCTION getNthHighestSalary(@N INT) RETURNS INT AS
+BEGIN
+    RETURN (
+        select TOP 1 salary FROM (
+select Salary
+,dense_rank() over (order by Salary DESC) ranking
+from Employee
+) t
+WHERE ranking = @N
+        
+    );
+END
+---------------------------------------------------------------------------------------------------------------------------------------
+--Question 11
+/*
+Write a SQL query to rank scores. If there is a tie between two scores, both should have the same ranking. Note that after a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no "holes" between ranks.
+
++----+-------+
+| Id | Score |
++----+-------+
+| 1  | 3.50  |
+| 2  | 3.65  |
+| 3  | 4.00  |
+| 4  | 3.85  |
+| 5  | 4.00  |
+| 6  | 3.65  |
++----+-------+
+For example, given the above Scores table, your query should generate the following report (order by highest score):
+
++-------+---------+
+| score | Rank    |
++-------+---------+
+| 4.00  | 1       |
+| 4.00  | 1       |
+| 3.85  | 2       |
+| 3.65  | 3       |
+| 3.65  | 3       |
+| 3.50  | 4       |
++-------+---------+
+Important Note: For MySQL solutions, to escape reserved words used as column names, you can use an apostrophe before and after the keyword. For example `Rank`.
+*/
+
+--Solution:
+select score, dense_rank() over(Order BY score DESC) As 'Rank'
+from Scores 
+---------------------------------------------------------------------------------------------------------------------------------------
+--Question 12:
+/*
+Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+id is the primary key for this table.
+ 
+
+Write an SQL query to find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The query result format is in the following example:
+
+ 
+
+Logs table:
++----+-----+
+| Id | Num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+
+Result table:
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+1 is the only number that appears consecutively for at least three times.
+*/
+
+--Solution 
+
+with cte as(
+select num, lead(num,1)over(order by id)as f1, lead(num,2)over(order by id)as f2
+from logs)
+
+select distinct num as ConsecutiveNums from cte 
+where num=f1 and f1=f2
+--------------------------------------------------------------------------------------------------------------------------------------
